8f6389e5c42c28933d1aa12e675a12300955fbde
Philip Mulford <peragorefalith1@gmail.com>
2013-01-06 20:55:38 -0500

    Basic Drive testing

--- src/edu/wpi/first/wpilibj/templates/drive.java
+++ src/edu/wpi/first/wpilibj/templates/drive.java
@@ -12,11 +12,14 @@ import edu.wpi.first.wpilibj.*;
  */
 public class drive {
     Team3373 testing = new Team3373();
-    Joystick drive = new Joystick(1);
-    boolean XBOX_A = drive.getRawButton(1);
-    double XBOX_LT = drive.getRawAxis(1);
- 
+    Joystick drivestick = new Joystick(1);
+    RobotDrive drive = new RobotDrive(1,2);
+    boolean XBOX_A = drivestick.getRawButton(1);
+    double XBOX_LSTICK = drivestick.getRawAxis(2);
+        
     public void Driving(){
-     
+    if  (XBOX_LSTICK >= -1 & XBOX_LSTICK <= 0 ){
+       
+    }  
  }
 }
