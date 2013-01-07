5f9a03ca05ef725ccfdd191e8b5a1215dd823cd2
Peragore <peragorefalith1@gmail.com>
2013-01-06 21:04:24 -0500

    Update src/edu/wpi/first/wpilibj/templates/drive.java
    fixed a typo

--- src/edu/wpi/first/wpilibj/templates/drive.java
+++ src/edu/wpi/first/wpilibj/templates/drive.java
@@ -14,7 +14,7 @@ public class drive {
     Team3373 testing = new Team3373();
     Joystick drivestick = new Joystick(1);
     RobotDrive drive = new RobotDrive(1,2);
-    boolean XDRIVE_A = drivestick.getRawButton(1);
+    boolean DRIVE_A = drivestick.getRawButton(1);
     double DRIVE_LSTICK = drivestick.getRawAxis(2);
         
     public void Driving(){
