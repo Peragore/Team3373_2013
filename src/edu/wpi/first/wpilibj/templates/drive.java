902e085921ce0968f345f3f0398b479c3149b032
Peragore <peragorefalith1@gmail.com>
2013-01-06 21:03:24 -0500

    Update src/edu/wpi/first/wpilibj/templates/drive.java
    changed variable names to fit 3373 naming convention

--- src/edu/wpi/first/wpilibj/templates/drive.java
+++ src/edu/wpi/first/wpilibj/templates/drive.java
@@ -14,11 +14,11 @@ public class drive {
     Team3373 testing = new Team3373();
     Joystick drivestick = new Joystick(1);
     RobotDrive drive = new RobotDrive(1,2);
-    boolean XBOX_A = drivestick.getRawButton(1);
-    double XBOX_LSTICK = drivestick.getRawAxis(2);
+    boolean XDRIVE_A = drivestick.getRawButton(1);
+    double DRIVE_LSTICK = drivestick.getRawAxis(2);
         
     public void Driving(){
-    if  (XBOX_LSTICK >= -1 & XBOX_LSTICK <= 0 ){
+    if  (DRIVE_LSTICK >= -1 & XBOX_LSTICK <= 0 ){
        
     }  
  }
