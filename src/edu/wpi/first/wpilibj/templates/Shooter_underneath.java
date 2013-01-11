32ae55a0c48fdd324574886011655b5ba5d7a224
Philip Mulford <peragorefalith1@gmail.com>
2013-01-10 23:04:53 -0500

    streamlined code. needs testing to see if better

--- src/edu/wpi/first/wpilibj/templates/Shooter_underneath.java
+++ src/edu/wpi/first/wpilibj/templates/Shooter_underneath.java
@@ -12,11 +12,13 @@ import edu.wpi.first.wpilibj.templates.Team3373;
  * @author Philip2
  */
 public class Shooter_underneath extends Shooter {
-    public void RPMTarget(){ //defines target based on input
+    public void RPMTarget(double a){ //defines target based on input. Appeaers to be better than speed increase. can probbaly be used in place of a bunch of code.
         if (shootA){
-            target = (RPMModifier *ShooterSpeedScale) + currentRPMT2;
+            target = ((RPMModifier *ShooterSpeedScale) + currentRPMT2) * a;
+            StageTwoTalon.set(target);
         } else if (shootB){
-            target = (( -RPMModifier * ShooterSpeedScale) + currentRPMT2);
+            target = (( -RPMModifier * ShooterSpeedScale) + currentRPMT2) * a;
+            StageTwoTalon.set(target);
         }
         
     }
