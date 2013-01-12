b68b8b26336869a63342b9f2ffa3fa1925cdf267
Jamie <Jamie@adm-tech-mpro.internal.highlandschool.org>
2013-01-12 11:30:32 -0500

    Added a way to change the percentage for stage 1 scaler Also added some println things for testing

--- src/edu/wpi/first/wpilibj/templates/Deadband.java
+++ src/edu/wpi/first/wpilibj/templates/Deadband.java
@@ -8,6 +8,18 @@ package edu.wpi.first.wpilibj.templates;
  *
  * @author Philip2
  */
-public class Deadband {
+public class Deadband{
     
+    
+    static int flag = 0;
+    
+    public static void Range (double target, double bottomNumber, double value, double increase){
+        while (target > value || flag != 1){
+            value += increase;
+            if (target <= value){
+                flag += 1;
+            }
+        }
+        
+    }
 }
