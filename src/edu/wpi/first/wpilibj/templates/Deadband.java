83bcc01c4635fa4ed02199938b4279a05effafca
RoboHawks <RoboHawks@10.0.1.58>
2013-01-13 14:24:32 -0500

    fixing shooter

--- src/edu/wpi/first/wpilibj/templates/Deadband.java
+++ src/edu/wpi/first/wpilibj/templates/Deadband.java
@@ -1,5 +1,7 @@
 package edu.wpi.first.wpilibj.templates;
 
+import java.lang.Math.*;
+
 public class Deadband{
     
     /*
@@ -14,7 +16,7 @@ public class Deadband{
         }
     }*/
     
-    public boolean joyStickDeadBand (double topNumber, double bottomNumber, double value){
+    public boolean zeroDeadBand (double topNumber, double bottomNumber, double value){
         boolean a = false;
         if (bottomNumber > value && value > topNumber){
          a = true;
@@ -23,4 +25,12 @@ public class Deadband{
             return a;
         }
     }
+    
+    public double fluxDeadBand (double value){
+        
+        double a = Math.round(value*100.0)/100.0;
+        return a;
+        
+    }
+    
 }
