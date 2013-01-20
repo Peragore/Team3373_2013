901d818cb6c5f87aa229b6206cba34b232527856
Philip Mulford <peragorefalith1@gmail.com>
2013-01-20 17:09:31 -0500

    Adding Spike Relays

--- src/edu/wpi/first/wpilibj/templates/PickArm.java
+++ src/edu/wpi/first/wpilibj/templates/PickArm.java
@@ -3,11 +3,27 @@
  * and open the template in the editor.
  */
 package edu.wpi.first.wpilibj.templates;
-
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.Relay.*;
+import edu.wpi.first.wpilibj.templates.*;
 /**
  *
  * @author Philip2
  */
 public class PickArm {
+    Team3373 team;
+    Relay ArmSpike = new Relay(1);
+    public PickArm(Team3373 t){
+       team = t;
+    }
     
+
+    
+    public void extend() {
+        if (team.shootLB){
+            ArmSpike.setDirection(Direction.kForward);
+        } else if (team.shootRB){
+            ArmSpike.setDirection(Direction.kReverse);
+        }
+    }
 }
