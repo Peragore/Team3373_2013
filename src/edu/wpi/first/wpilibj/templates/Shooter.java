c2d2862ca6a0ecf2d74b8c5e19feab8d66dc8947
RoboHawks <RoboHawks@10.0.1.58>
2013-01-20 16:34:32 -0500

    cleaned up Team3373 by adding governors to Shooter3373

--- src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -29,18 +29,22 @@ public class Shooter {
     }
     public double increaseSpeed(double a){ //increases stage 2 by 1/10 of possible speed
         a += 0.1;
+        if (a >= 1) {a = 1;}
         return a;
     }
     public double decreaseSpeed(double a){//decreases stage 2 by 1/10 of possible speed
         a -= 0.1;
+        if (a <= 0){ a = 0;}  
         return a;
     }
     public double increasePercentage(double a){//increases percentage of what stage 2 is multiplyied by to get 1
         a += 0.05;
+        if (a >= 1) {a = 1;}
         return a;
     }
     public double decreasePercentage(double a){//decreases percentage of what stage 2 is multiplyied by to get 1
         a -= 0.05;
+        if (a <= 0 ) {a = 0;}
         return a;
     }
     public double stop(){
