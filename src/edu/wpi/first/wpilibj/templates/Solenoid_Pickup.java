9c7f6cb6bb2fbd162d84ba5f213b89e3d87964bf
RoboHawks <RoboHawks@10.0.1.58>
2013-01-21 16:59:02 -0500

    solenoid functionality

--- src/edu/wpi/first/wpilibj/templates/Solenoid_Pickup.java
+++ src/edu/wpi/first/wpilibj/templates/Solenoid_Pickup.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package edu.wpi.first.wpilibj.templates;
+import edu.wpi.first.wpilibj.DriverStationLCD.Line;
 
 /**
  *
@@ -10,4 +11,17 @@ package edu.wpi.first.wpilibj.templates;
  */
 public class Solenoid_Pickup {
     
-}
+   Team3373 team;
+   
+   public Solenoid_Pickup(Team3373 t){
+       team = t;
+   }
+    
+    public void solenoid(){
+        
+        if (team.shootA){
+            team.grabSolenoid.set(true);
+        } else if (team.shootB)
+            team.grabSolenoid.set(false);
+        }
+    }
