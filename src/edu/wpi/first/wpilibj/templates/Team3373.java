f162d08d71ed7a05f061f706e588981a7503febc
Philip Mulford <peragorefalith1@gmail.com>
2013-01-26 18:01:44 -0500

    Revert "state machine get"
    This reverts commit 94e49b4b55ae4274e131de69703aea47b57a870e.

--- src/edu/wpi/first/wpilibj/templates/Team3373.java
+++ src/edu/wpi/first/wpilibj/templates/Team3373.java
@@ -243,6 +243,7 @@ public class Team3373 extends SimpleRobot{
         Arm.armUp();
         Arm.armDown();
         Arm.goToPosition(2.5);
+        objShooter.elevator();
         /*
         //try {Thread.sleep(1000);} catch(Exception e){}
         //String percentage = Double.toString();
